fe69a37914900adca8cf897283e6c5a2148e01ae
Yurij2015 <nabor_2008@i.ua>
2019-10-30 20:31:24 +0300

    sql

---
+++ infocomplect.sql
@@ -0,0 +1,224 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 30 2019 г., 20:29
+-- Версия сервера: 5.5.62
+-- Версия PHP: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `infocomplect`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `idcategory` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`idcategory`, `name`) VALUES
+(1, 'бьбьбьб');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customers`
+--
+
+CREATE TABLE `customers` (
+  `idcustomer` int(11) NOT NULL,
+  `name` varchar(145) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `managers`
+--
+
+CREATE TABLE `managers` (
+  `idmanager` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `idorder` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `managers_idmanager` int(11) NOT NULL,
+  `customers_idcustomer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `idproduct` int(11) NOT NULL,
+  `name` varchar(65) NOT NULL,
+  `weight` float DEFAULT NULL COMMENT 'Вес в кг',
+  `volume` float DEFAULT NULL COMMENT 'Объем в куб. м',
+  `count` int(11) DEFAULT NULL COMMENT 'Количество в шт',
+  `category_idcategory` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `productsoforders`
+--
+
+CREATE TABLE `productsoforders` (
+  `idprodoforder` int(11) NOT NULL,
+  `count` float NOT NULL COMMENT 'В единицах товара',
+  `product_idproduct` int(11) NOT NULL,
+  `orders_idorder` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`idcategory`);
+
+--
+-- Индексы таблицы `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`idcustomer`);
+
+--
+-- Индексы таблицы `managers`
+--
+ALTER TABLE `managers`
+  ADD PRIMARY KEY (`idmanager`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`idorder`),
+  ADD KEY `fk_orders_managers1_idx` (`managers_idmanager`),
+  ADD KEY `fk_orders_customers1_idx` (`customers_idcustomer`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`idproduct`),
+  ADD KEY `fk_product_category_idx` (`category_idcategory`);
+
+--
+-- Индексы таблицы `productsoforders`
+--
+ALTER TABLE `productsoforders`
+  ADD PRIMARY KEY (`idprodoforder`),
+  ADD KEY `fk_prodoforder_product1_idx` (`product_idproduct`),
+  ADD KEY `fk_productsoforders_orders1_idx` (`orders_idorder`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `idcategory` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `idcustomer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `managers`
+--
+ALTER TABLE `managers`
+  MODIFY `idmanager` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `idorder` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `idproduct` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `productsoforders`
+--
+ALTER TABLE `productsoforders`
+  MODIFY `idprodoforder` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_orders_customers1` FOREIGN KEY (`customers_idcustomer`) REFERENCES `customers` (`idcustomer`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_orders_managers1` FOREIGN KEY (`managers_idmanager`) REFERENCES `managers` (`idmanager`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_product_category` FOREIGN KEY (`category_idcategory`) REFERENCES `categories` (`idcategory`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ограничения внешнего ключа таблицы `productsoforders`
+--
+ALTER TABLE `productsoforders`
+  ADD CONSTRAINT `fk_prodoforder_product1` FOREIGN KEY (`product_idproduct`) REFERENCES `products` (`idproduct`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_productsoforders_orders1` FOREIGN KEY (`orders_idorder`) REFERENCES `orders` (`idorder`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
